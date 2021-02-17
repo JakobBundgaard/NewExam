ace95aadf17a1dc9f05919cc9aa902e9e5a847d3
JakobBundgaard <jakobbundgaard@hotmail.com>
2021-02-17 19:49:16 +0100

    ny commit

--- src/main/java/com/bundgaard/NewExam/controllers/InternController.java
+++ src/main/java/com/bundgaard/NewExam/controllers/InternController.java
@@ -1,23 +1,28 @@
 package com.bundgaard.NewExam.controllers;
 
 
+import com.bundgaard.NewExam.model.Intern;
 import com.bundgaard.NewExam.service.CompanyService;
 import com.bundgaard.NewExam.service.InternService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api")
 public class InternController {
 
     @Autowired
     private InternService internService;
-    @Autowired
-    private CompanyService companyService;
 
-    @GetMapping("/api/showInterns")
-    public ResponseEntity showAllInterns(){
-        return ResponseEntity.ok(internService.findAll());
+
+    @GetMapping("/interns")
+    public List<Intern> getAllInterns(){
+        System.out.println(internService.findAll());
+        return internService.findAll();
     }
 }
--- src/main/java/com/bundgaard/NewExam/service/springdatajpa/InternJPA.java
+++ src/main/java/com/bundgaard/NewExam/service/springdatajpa/InternJPA.java
@@ -1,19 +1,30 @@
 package com.bundgaard.NewExam.service.springdatajpa;
 
 import com.bundgaard.NewExam.model.Intern;
+import com.bundgaard.NewExam.repositories.InternRepo;
 import com.bundgaard.NewExam.service.InternService;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 @Service
 public class InternJPA implements InternService {
+
+
+    private final InternRepo internRepo;
+
+    public InternJPA(InternRepo internRepo) {
+        this.internRepo = internRepo;
+    }
+
+
     @Override
     public List<Intern> findAll() {
         List<Intern>  list = new ArrayList<>();
+        internRepo.findAll().forEach(list::add);
+        System.out.println(list);
         return list;
     }
 
