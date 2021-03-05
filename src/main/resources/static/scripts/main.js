cf475e8ba685b893daf849640bdb02fe97532287
JakobBundgaard <jakobbundgaard@hotmail.com>
2021-03-05 10:54:22 +0100

    Create works... Finally

--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -1,11 +1,9 @@
 function deleteIntern(id) {
-
     console.log(id)
     $.ajax({
         url: 'http://localhost:8080/api/interns/' + id,
         method: 'DELETE',
         success: function() {
-
             getAllInterns();
         },
         error: function(error) {
@@ -14,60 +12,44 @@ function deleteIntern(id) {
     })
 }
 
-// Create a new student
+// Create a new intern
 function createIntern() {
 
-    var intern = {};
-    var company = {};
-    var dynamicURL = "";
-    var methodName = "";
+    let intern = {};
+    let dynamicURL = "";
+    let methodName = "";
 
     $('#btnAddIntern').click(function() {
-        alert("2")
-        intern.firstname = $('#firstname').val();
-        intern.lastname = $('#lastname').val();
-        //intern.email = $('#finishedintern').val();
+        intern.firstName = $('#firstname').val();
+        intern.lastName = $('#lastname').val();
         intern.isInternshipDone = $('#finishedintern').val();
-        intern.assignedCompany = $('')
-        intern.company = company;
-        alert("3")
         dynamicURL = "http://localhost:8080/api/newIntern/";
         methodName = "POST";
-        alert("4")
-        var internObj = JSON.stringify(intern);
+        let internObj = JSON.stringify(intern);
         console.log(internObj);
         $.ajax({
             url: "http://localhost:8080/api/newIntern/",
             method: "POST",
-            contentType: 'application/json; charset=utf-8',
+            contentType: 'application/JSON',
             data: JSON.stringify(intern),
             success: function() {
                 console.log(methodName);
-                //resetForm();
                 getAllInterns();
             },
             error: function(error) {
-                alert("A error have occured - Please do something else. Error Message: " + error);
+                alert("A error occured: " + error);
             }
         })
-
     })
-
 }
-
-
-
 
 // Get all Interns
 function getAllInterns() {
-
     $.ajax({
         method: "GET",
         url: 'http://localhost:8080/api/interns'
     }).done(function(data) {
-
         $('#tdata').empty();
-
         $.each(data, function(i, interns) {
             // Loading data into table
             $('#tdata')
@@ -96,10 +78,7 @@ function getAllInterns() {
         });
     })
 }
-
-
 $(document).ready(function() {
     getAllInterns();
     createIntern()
-
 });
